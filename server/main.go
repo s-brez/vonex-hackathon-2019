ece11a4af1fac6c4001af5284cc648818ec9228b
s-brez <sam@sdbgroup.io>
2019-09-26 13:20:32 +1000

    Initial commit - Server skeleton
    - Start adding API endpoints to main.go
    - Empty index.html and index.js added

---
+++ server/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+// Item object
+type Item struct {
+	ID        	primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Name 		string           	`json:"name,omitempty" bson:"name,omitempty"`
+	Description string          	`json:"description,omitempty" bson:"description,omitempty"`
+	Stock 		string           	`json:"stock,omitempty" bson:"stock,omitempty"`
+}
+
+// User object
+type User struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+	Password  string             `json:"password,omitempty" bson:"lastname,omitempty"`
+}
+
+// TBC. Maybe get all purchases for a user.
+func ExtraAPIEndpoint(response http.ResponseWriter, request *http.Request) {}
+
+// Get all inventory
+func GetAllInventoryEndpoint(response http.ResponseWriter, request *http.Request) {}
+
+// Return items that match search term
+func FindInventoryItemEndpoint(response http.ResponseWriter, request *http.Request) {}
+
+// Permanenty remove an item from database
+func DeleteInventoryEndpoint(response http.ResponseWriter, request *http.Request) {}
+
+// Increase or decrease inventory levels for given item
+func ModifyInventoryEndpoint(response http.ResponseWriter, request *http.Request) {}
+
+// Create a new user, write to db collection "users"
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("hackathon_app").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+// Get all users
+func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var users []User
+	collection := client.Database("hackathon_app").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var user User
+		cursor.Decode(&user)
+		users = append(users, user)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(users)
+}
+
+// Return user where id matches user[id]
+func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user User
+	collection := client.Database("hackathon_app").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(user)
+}
+
+func main() {
+	fmt.Println("Starting server...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/user", CreateUserEndpoint).Methods("POST")
+	router.HandleFunc("/users", GetUsersEndpoint).Methods("GET")
+	router.HandleFunc("/user/{id}", GetUserEndpoint).Methods("GET")
+	http.ListenAndServe(":5005", router)
+}

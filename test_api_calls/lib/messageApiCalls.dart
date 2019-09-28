bf6b3ba38b7086842c326a5c5cbac241c8934ae7
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-09-29 01:43:54 +1000

    Finish API calls for messages, items, listings, users api calls. Has not debug these features.

--- test_api_calls/lib/messageApiCalls.dart
+++ test_api_calls/lib/messageApiCalls.dart
@@ -8,132 +8,136 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-[{"_id":"5d8f6148f5ede19064ed2619","Time":"","to":"alice@gmail.com","from":"steve@gmail.com","text":"Hi Alice, I am interested in your Vintage Lamp item, would you consider a trade for my toaster oven?"},{"_id":"5d8f6148f5ede19064ed261a","Time":"","to":"steve@gmail.com","from":"alice@gmail.com","text":"Steve, I might like your toaster oven, could you please list it here on Up-Cycle via 'Add Listing' so I can have a little more info?"}]
 
-String resource = 'user';
+String resource = 'messages';
 String apiUrl = 'http://localhost:5005/';
-User mainUser;
+Message allMessages;
 
-// GET
-Future<User> fetchUser(String email, {bool setAsMainUser = false}) async {
-  String url = apiUrl + resource + '/' + email;
+
+
+// GET all messages in the db
+Future<Message> fetchAllMessages() async {
+  String url = apiUrl + resource;
   final response =
       await http.get(url);
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON.
-    if (setAsMainUser){
-      mainUser = User.fromJson(json.decode(response.body));
-      print('Successfully fetch user ' + email);
-      print(response.body);
-      return mainUser;
-    }
-    return User.fromJson(json.decode(response.body));
+    print('Successfully fetch all messages');
+    print(response.body);
+
+    return Message.fromJson(json.decode(response.body));
   } else {
     // If that call was not successful, throw an error.
-    throw Exception('Failed to fetch user ' + email);
+    throw Exception('Failed to fetch all messages in the db');
+  }
+}
+
+// GET
+Future<Message> fetchMessage(String emailSender, {String emailReceiver, int loadLimit = 1000}) async {
+  String url = apiUrl + resource + '/' + emailSender;
+  final response =
+      await http.get(url);
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+      print('Successfully fetch messages sent by' + emailSender);
+      print(response.body);
+
+    return Message.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to fetch messages sent by' + emailSender);
   }
 }
 
 // POST
-Future<User> createUser(Map<String, dynamic> body ) async {
-  String email = body['email'];
+Future<Message> createMessage(Map<String, dynamic> body ) async {
+  String toEmail = body['to'];
+  String fromEmail = body['from'];
   String url = apiUrl + resource;
   final response =
       await http.put(url, body: body);
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON.
-    print('Successfully create user ' + email);
+    print('Successfully create messages sent by ' + fromEmail + ' to ' + toEmail);
     print(response.body);
-    return User.fromJson(json.decode(response.body));
+    return Message.fromJson(json.decode(response.body));
   } else {
     // If that call was not successful, throw an error.
-    throw Exception('Failed to create user ' + body['email']);
+    throw Exception('Failed to create messages sent by ' + fromEmail + ' to ' + toEmail);
   }
 }
 
-// PATCH
-Future<User> updateUser(Map<String, dynamic> body ) async {
-  String email = body['email'];
-  String url = apiUrl + resource + '/' + email;
-  final response =
-      await http.patch(url, body: body);
+// PATCH - No patch method. Messages cannot be edited.
+// Future<Message> updateMessage(Map<String, dynamic> body ) async {
+//   String toEmail = body['to'];
+//   String fromEmail = body['from'];
+//   String url = apiUrl + resource + '/' + email;
+//   final response =
+//       await http.patch(url, body: body);
 
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON.
-    print('Successfully update user ' + email);
-    print(response.body);
-    return User.fromJson(json.decode(response.body));
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to update user ' + email);
-  }
-}
+//   if (response.statusCode == 200) {
+//     // If the call to the server was successful, parse the JSON.
+//     print('Successfully update messages ' + email);
+//     print(response.body);
+//     return Message.fromJson(json.decode(response.body));
+//   } else {
+//     // If that call was not successful, throw an error.
+//     throw Exception('Failed to update messages ' + email);
+//   }
+// }
 
-// DELETE
-Future<User> deleteUser(Map<String, dynamic> body) async {
-  String email = body['email'];
-  String url = apiUrl + resource + '/' + email;
-  final response =
-      await http.delete(url);
+// DELETE - probably not necessary to have
+// Future<Message> deleteMessage(Map<String, dynamic> body) async {
+//   String email = body['email'];
+//   String url = apiUrl + resource + '/' + email;
+//   final response =
+//       await http.delete(url);
 
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON.
-    print('Successfully delete user ' + email);
-    return User.fromJson(json.decode(response.body));
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to delete user ' + email);
-  }
-}
+//   if (response.statusCode == 200) {
+//     // If the call to the server was successful, parse the JSON.
+//     print('Successfully delete messages ' + email);
+//     return Message.fromJson(json.decode(response.body));
+//   } else {
+//     // If that call was not successful, throw an error.
+//     throw Exception('Failed to delete messages ' + email);
+//   }
+// }
 
-class User {
-  String email;
-  String firstname;
-  String lastname;
-  String password;
-  bool picker;
-  bool packer;
-  String fb;
-  String insta;
-  List<String> savedItems;
-  List<String> listings; 
+// JSon Structure of a message
+// [
+//   {
+// "_id":"5d8f6148f5ede19064ed2619",
+// "Time":"",
+// "to":"alice@gmail.com",
+// "from":"steve@gmail.com",
+// "text":"Hi Alice, I am interested in your Vintage Lamp item, would you consider a trade for my toaster oven?"},
 
-  User({this.email, this.firstname, this.lastname, this.password, this.picker, this.packer, this.fb, this.insta, this.savedItems, this.listings});
+// {"_id":"5d8f6148f5ede19064ed261a",
+// "Time":"",
+// "to":"steve@gmail.com",
+// "from":"alice@gmail.com",
+// "text":"Steve, I might like your toaster oven, could you please list it here on Up-Cycle via 'Add Listing' so I can have a little more info?"
+// }
+// ]
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      firstname: json['firstname'],
-      lastname: json['lastname'],
-      password: json['password'],
-      picker: json['picker'],
-      packer: json['packer'],
-      fb: json['fb'],
-      insta: json['insta'],
-      savedItems: json['savedItems'],
-      listings: json['listings'],
+class Message {
+  final String Time;
+  final String to;
+  final String from;
+  final String text;
+
+  Message({this.Time, this.to, this.from, this.text});
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      Time: json['Time'],
+      to: json['to'],
+      from: json['from'],
+      text: json['text'],
     );
-  }
-
-  void updatePassword(String newPassword){
-    password = newPassword;
-  }
-
-  void addSavedItem(String newItem){
-    savedItems.add(newItem);
-  }
-
-  bool removeSavedItem(String toRemoved){
-    return savedItems.remove(toRemoved);
-  }
-
-  void addListings(String newListing){
-    listings.add(newListing);
-  }
-
-  bool removeListings(String toRemoved){
-    return listings.remove(toRemoved);
   }
 }
 
